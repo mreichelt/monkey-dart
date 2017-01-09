913131453d87752bdd2dde519209ddfdcd926550
Marc Reichelt <mcreichelt@gmail.com>
2017-01-09 13:08:19 +0100

    remove hello world code

--- test/monkey_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:monkey_dart/monkey.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test("sameNumber returns same number 42", () {
-    expect(sameNumber(42), 42);
-  });
-}
