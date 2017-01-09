ae669af63c05425d9c07ea85ffcd1136481300dc
Marc Reichelt <mcreichelt@gmail.com>
2017-01-09 13:17:58 +0100

    dummy commit to trigger first Travis build

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,12 +3,16 @@
 # Fast fail the script on failures.
 set -e
 
+
+
 # Verify that dartfmt has been run
 echo "Checking dartfmt..."
 if [[ $(dartfmt -n --set-exit-if-changed lib/ test/) ]]; then
 	echo "Failed dartfmt check: run dartfmt -w lib/ test/"
 	exit 1
 fi
+
+
 
 # Run the tests.
 echo "Running tests..."
